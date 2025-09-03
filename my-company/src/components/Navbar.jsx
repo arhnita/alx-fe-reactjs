f42c96ed0fc8fded1b63ce9e9db09704c391ea10
Anita Samuel <migist2@gmail.com>
2025-09-03 23:48:11 +0100

    navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,53 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function Navbar() {
   return (
-    <nav style={{
-      backgroundColor: '#333',
-      padding: '1rem',
-      position: 'sticky',
-      top: '0',
-      zIndex: '100'
-    }}>
-      <div style={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        maxWidth: '1200px',
-        margin: '0 auto'
-      }}>
-        <div style={{
-          color: 'white',
-          fontSize: '1.5rem',
-          fontWeight: 'bold'
-        }}>
-          MyCompany
-        </div>
-        <div>
-          <a href="#home" style={{
-            color: 'white',
-            textDecoration: 'none',
-            margin: '0 1rem',
-            fontSize: '1.1rem'
-          }}>Home</a>
-          <a href="#about" style={{
-            color: 'white',
-            textDecoration: 'none',
-            margin: '0 1rem',
-            fontSize: '1.1rem'
-          }}>About</a>
-          <a href="#services" style={{
-            color: 'white',
-            textDecoration: 'none',
-            margin: '0 1rem',
-            fontSize: '1.1rem'
-          }}>Services</a>
-          <a href="#contact" style={{
-            color: 'white',
-            textDecoration: 'none',
-            margin: '0 1rem',
-            fontSize: '1.1rem'
-          }}>Contact</a>
-        </div>
-      </div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/contact">Contact</Link>
     </nav>
   );
 }
